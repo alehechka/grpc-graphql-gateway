f2bcf08ea175ada0697646a2de0d2b6f6261b634
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2022-07-28 14:39:09 -0500

    add descriptions to gql Money

--- ptypes/google/type/money/money.go
+++ ptypes/google/type/money/money.go
@@ -19,15 +19,25 @@ func Gql__type_Money() *graphql.Object {
 			Name: "Google_type_Money",
 			Fields: graphql.Fields{
 				"currencyCode": &graphql.Field{
-					Type: graphql.String,
+					Type:        graphql.String,
+					Description: `The three-letter currency code defined in ISO 4217.`,
 				},
 				"units": &graphql.Field{
 					Type: graphql.Int,
+					Description: `The whole units of the amount.
+For example if ` + "`" + `currencyCode` + "`" + ` is ` + "`" + `"USD"` + "`" + `, then 1 unit is one US dollar.`,
 				},
 				"nanos": &graphql.Field{
 					Type: graphql.Int,
+					Description: `Number of nano (10^-9) units of the amount.
+The value must be between -999,999,999 and +999,999,999 inclusive.
+If ` + "`" + `units` + "`" + ` is positive, ` + "`" + `nanos` + "`" + ` must be positive or zero.
+If ` + "`" + `units` + "`" + ` is zero, ` + "`" + `nanos` + "`" + ` can be positive, zero, or negative.
+If ` + "`" + `units` + "`" + ` is negative, ` + "`" + `nanos` + "`" + ` must be negative or zero.
+For example $-1.75 is represented as ` + "`" + `units` + "`" + `=-1 and ` + "`" + `nanos` + "`" + `=-750,000,000.`,
 				},
 			},
+			Description: `Represents an amount of money with its currency type.`,
 		})
 	}
 	return gql__type_Money
@@ -39,15 +49,25 @@ func Gql__input_Money() *graphql.InputObject {
 			Name: "Google_input_Money",
 			Fields: graphql.InputObjectConfigFieldMap{
 				"currencyCode": &graphql.InputObjectFieldConfig{
-					Type: graphql.String,
+					Type:        graphql.String,
+					Description: `The three-letter currency code defined in ISO 4217.`,
 				},
 				"units": &graphql.InputObjectFieldConfig{
 					Type: graphql.Int,
+					Description: `The whole units of the amount.
+For example if ` + "`" + `currencyCode` + "`" + ` is ` + "`" + `"USD"` + "`" + `, then 1 unit is one US dollar.`,
 				},
 				"nanos": &graphql.InputObjectFieldConfig{
 					Type: graphql.Int,
+					Description: `Number of nano (10^-9) units of the amount.
+The value must be between -999,999,999 and +999,999,999 inclusive.
+If ` + "`" + `units` + "`" + ` is positive, ` + "`" + `nanos` + "`" + ` must be positive or zero.
+If ` + "`" + `units` + "`" + ` is zero, ` + "`" + `nanos` + "`" + ` can be positive, zero, or negative.
+If ` + "`" + `units` + "`" + ` is negative, ` + "`" + `nanos` + "`" + ` must be negative or zero.
+For example $-1.75 is represented as ` + "`" + `units` + "`" + `=-1 and ` + "`" + `nanos` + "`" + `=-750,000,000.`,
 				},
 			},
+			Description: `Represents an amount of money with its currency type.`,
 		})
 	}
 	return gql__input_Money
